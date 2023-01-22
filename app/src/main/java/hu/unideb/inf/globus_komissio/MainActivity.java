62be4b3b712cc28ab065d7a0f42a76e3df39da87
misi0 <galmihaly118@gmail.com>
2023-01-22 23:03:01 +0100

    Initial commit

---
+++ app/src/main/java/hu/unideb/inf/globus_komissio/MainActivity.java
@@ -0,0 +1,18 @@
+package hu.unideb.inf.globus_komissio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.fragmentsLayout, new ProgramStartFragment()).commit();
+    }
+}
