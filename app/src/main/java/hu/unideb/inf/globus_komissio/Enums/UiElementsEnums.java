ff79619fd4bbe9140058f2193aea849c3d0abae6
misi0 <galmihaly118@gmail.com>
2023-01-30 01:09:02 +0100

    room5

---
+++ app/src/main/java/hu/unideb/inf/globus_komissio/Enums/UiElementsEnums.java
@@ -0,0 +1,20 @@
+package hu.unideb.inf.globus_komissio.Enums;
+
+public enum UiElementsEnums {
+
+    LEFT_ARROW_1,
+    PROGRESS_BAR_1,
+    READY_STATE_1,
+
+    LEFT_ARROW_2,
+    PROGRESS_BAR_2,
+    READY_STATE_2,
+
+    LEFT_ARROW_3,
+    PROGRESS_BAR_3,
+    READY_STATE_3,
+
+    LEFT_ARROW_4,
+    PROGRESS_BAR_4,
+    READY_STATE_4
+}
